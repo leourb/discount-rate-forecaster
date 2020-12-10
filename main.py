b48dd1b6be41c23da0804697f4a47b8f5c3d36b4
Leonardo Urbano <urbanstyle_87@hotmail.it>
2020-12-10 12:15:01 +0100

    Sample usage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 """Main module that executes the script"""
 
-from data_downloader import DataDownloader
 from fair_price_calculator import FairPriceCalc
 
 if __name__ == "__main__":
+    print(FairPriceCalc("IBM").fair_price)
