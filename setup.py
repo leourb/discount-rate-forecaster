8e8d14f21cf5aabdd782c33d1d5174fa5d7fe219
Leonardo Urbano <urbanstyle_87@hotmail.it>
2020-12-20 18:52:24 +0100

    Setup to install the file

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 import setuptools
 
-with open("README.md", "r") as fh:
+with open("ggm_calculator/README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="GGM-Calculator",
+    name="ggm_calculator",
     version="1.0.0",
     author="Leonardo Urbano",
     author_email="leonardo.urbano87@libero.it",
@@ -12,6 +12,7 @@ setuptools.setup(
     description="Gordon Growth Model Calculator",
     long_description=long_description,
     long_description_content_type="text/markdown",
+    include_package_data=True,
     url="https://github.com/leourb",
     classifiers=[
         "Programming Language :: Python :: 3",
