54481a04ab02cb197eccc24c2d79e86162426a74
Leonardo Urbano <urbanstyle_87@hotmail.it>
2020-12-22 11:13:51 +0100

    Remove prints and value returned from __init__

--- ggm_calculator/infer_parameters.py
+++ ggm_calculator/infer_parameters.py
@@ -16,7 +16,7 @@ class InferParameters:
         if self.__inputs.get_estimated_fair_price() is None:
             self.__infer_g = None
             self.__infer_r = None
-            return None
+            return
         self.__last_price = self.__inputs.get_inputs().get_ticker_data().tail(1)["Adj Close"].values[0]
         self.__dividend_growth = self.__inputs.get_inputs().get_dividend_growth()
         self.__dividend_data = self.__inputs.get_inputs().get_dividend_data()
@@ -33,8 +33,6 @@ class InferParameters:
         yearly_dividend = float(self.__dividend_data["Dividend Amount"].head(4).sum())
         expected_dividend = yearly_dividend * (1 + self.__dividend_growth)
         growth = self.__required_return - (expected_dividend / self.__last_price)
-        print(f"The estimated growth for {self.__ticker} is {growth}.")
-        print(f"Inputs: Price={self.__last_price}, r={self.__required_return}, D1={expected_dividend}")
         return growth
 
     def __infer_required_return_from_mkt_price(self):
@@ -46,8 +44,6 @@ class InferParameters:
         yearly_dividend = float(self.__dividend_data["Dividend Amount"].head(4).sum())
         expected_dividend = yearly_dividend * (1 + self.__dividend_growth)
         required_return = (expected_dividend/self.__last_price) + self.__dividend_growth
-        print(f"The estimated required return for {self.__ticker} is {required_return}.")
-        print(f"Inputs: Price={self.__last_price}, g={self.__dividend_growth}, D1={expected_dividend}")
         return required_return
 
     def get_inferred_g(self):
